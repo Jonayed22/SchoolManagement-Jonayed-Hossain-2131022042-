c10b10f813e70a3e796f72f388c025c911130e84
Jonayed Hossain <jonayedhossain235@gmail.com>
2025-08-02 12:16:43 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,6 @@ $database = "school_management_system";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
