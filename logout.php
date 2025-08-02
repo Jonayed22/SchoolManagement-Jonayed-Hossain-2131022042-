fd7bf0ad950befb69e8b8018dc07d7f5ca952625
Jonayed Hossain <jonayedhossain235@gmail.com>
2025-08-02 12:21:06 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,8 +4,8 @@ session_unset();
 session_destroy();
 
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-header("Pragma: no-cache"); // HTTP 1.0.
-header("Expires: 0"); // Proxies.
+header("Pragma: no-cache"); 
+header("Expires: 0"); 
 
 header('Location: login.php');
 exit();
